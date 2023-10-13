7663ae0aaf05bf38e568db37fe2811a839125cbe
Sukanya Kokani <91522376+sukanya2303@users.noreply.github.com>
2023-10-13 23:57:17 +0530

    Add files via upload
    added

--- php_mailer/mail.php
+++ php_mailer/mail.php
@@ -1,49 +1,45 @@
 <?php
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-//Load Composer's autoloader
-require 'vendor/autoload.php';
+require 'vendor/autoload.php'; // Include PHPMailer
 
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
-
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 's.kokani2303@gmail.com';                     //SMTP username
-    $mail->Password   = 'vwdy ooxo yywz eipa';                               //SMTP password
-    $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
-    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-    //Recipients
-    $mail->setFrom('s.kokani2303@gmail.com', 'WebFusion');
-    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-    $mail->addAddress('ellen@example.com');               //Name is optional
-    $mail->addReplyTo('info@example.com', 'Information');
-   
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $phone = $_POST['phone'];
     $message = $_POST['message'];
 
-    //Attachments
-    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+    // Create a new PHPMailer instance
+    $mail = new PHPMailer(true);
 
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'New Message';
-    $mail->Body    = $message;
+    try {
+        //Server settings
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com'; // Your SMTP server
+        $mail->SMTPAuth = true;
+        $mail->Username = 's.kokani2303@gmail.com'; // Your SMTP username
+        $mail->Password = 'vwdy ooxo yywz eipa'; // Your SMTP password
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
 
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        //Recipients
+        $mail->setFrom($email, $name);
+        $mail->addAddress('s.kokani2303@gmail.com'); // Change to your recipient's email and name
+
+        //Content
+        $mail->isHTML(false);
+        $mail->Subject = 'Contact Form Submission';
+        $mail->Body = "Name: $name\nEmail: $email\nPhone: $phone\nMessage:\n$message";
+
+        $mail->send();
+        echo 'Message has been sent. Thank you for contacting us!';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+} else {
+    // Handle form not being submitted
+    echo "Form not submitted.";
 }
+?>
