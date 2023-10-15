a79e19d9f4d7bf53c722197a1da4cbfbd11d4134
Sukanya Kokani <91522376+sukanya2303@users.noreply.github.com>
2023-10-16 00:33:58 +0530

    Add files via upload
    added php

--- php_mailer/contact-form.php
+++ php_mailer/contact-form.php
@@ -1,63 +1,46 @@
-<?php use PHPMailer\PHPMailer\PHPMailer;
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-//Load Composer's autoloader
-require 'vendor/autoload.php';
-$mail = new PHPMailer(true);
+require 'vendor/autoload.php'; // Include PHPMailer
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
 
-		
-	if(isset($_POST['submit_form']))
-			{ 
-          $to=$_POST['to']; 
-       $subject=$_POST['subject']; 
-        $msg=$_POST['msg']; 
-        $name=$_POST['name']; 
+    // Create a new PHPMailer instance
+    $mail = new PHPMailer(true);
 
-try {
-    //Server settings
-    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 's.kokani2303@gmail.com';                     //SMTP username
-    $mail->Password   = 'vwdy ooxo yywz eipa';                               //SMTP password
-    $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
-    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    try {
+        //Server settings
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com'; // Your SMTP server
+        $mail->SMTPAuth = true;
+        $mail->Username = 's.kokani2303@gmail.com'; // Your SMTP username
+        $mail->Password = 'vwdy ooxo yywz eipa'; // Your SMTP password
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
 
-    
-    $mail->setFrom('noreply@gmail.com', 'Web Fusion');
-    
-    //for multiple emails
-    // $toarray=explode(",",$to);
-    // foreach($toarray as $row)
-    // {
-    //      $mail->addAddress($row); 
-    // }
-    //for single email 
-       $mail->addAddress($to,$name);     //Add a recipient              //Name is optional
-       
-    $mail->addReplyTo('s.kokani2303@gmail.com', 'Wen Fusion');
-    //$mail->addCC('cc@example.com');
-    //$mail->addBCC('bcc@example.com');
+        //Recipients
+        $mail->setFrom($email, $name);
+        $mail->addAddress('s.kokani2303@gmail.com'); // Change to your recipient's email and name
 
-    //Attachments
-    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+        //Content
+        $mail->isHTML(false);
+        $mail->Subject = 'WebFusion Solutions';
+        $mail->Body = "Name: $name\nEmail: $email\nPhone: $phone\nMessage:\n$message";
 
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = $subject;
-    $mail->Body    = $msg;
-    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    $mail->send();
-    echo '<div class="success ">Message has been sent</div>';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
-
+        $mail->send();
+        echo 'Message has been sent. Thank you for contacting us!';
+        echo '<a href="../index.html">Click here to return to the homepage</a>';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
+} else {
+    // Handle form not being submitted
+    echo "Form not submitted.";
+}
 ?>
-	
